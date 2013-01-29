133db5950e189c78275648e0667189f46a8ee293
kirtley <kirtley@kirtley-zbox>
2013-01-28 22:41:50 -0800

    .

--- use-wget-proxy.sh
+++ use-wget-proxy.sh
@@ -3,8 +3,10 @@ wget_proxy=192.168.11.3:3128
 
 function configure_wget_proxy ()
 {
-sudo sed -i 's/#http_proxy = http:\/\/proxy.yoyodyne.com:18023\//#http_proxy = http:\/\/$wget_proxy\//g' /etc/wgetrc
-sudo sed -i 's/#ftp_proxy = http:\/\/proxy.yoyodyne.com:18023\//ftp_proxy = http:\/\/$wget_proxy\//g'  /etc/wgetrc
-sudo sed -i 's/#use_proxy = on/use_proxy = on/g' /etc/wgetrc
+sudo cp /etc/wgetrc /etc/wgetrc.orig
+sudo chmod a-w /etc/wgetrc.orig
+sudo sed -i "s/#http_proxy = http:\/\/proxy.yoyodyne.com:18023\//http_proxy = http:\/\/$wget_proxy\//g" /etc/wgetrc
+sudo sed -i "s/#ftp_proxy = http:\/\/proxy.yoyodyne.com:18023\//ftp_proxy = http:\/\/$wget_proxy\//g"  /etc/wgetrc
+sudo sed -i "s/#use_proxy = on/use_proxy = on/g" /etc/wgetrc
 }
 configure_wget_proxy
