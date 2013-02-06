10a320f7f2dcfdb6bed8906ffae7956bbd435224
kirtley <kirtley@kirtley-zbox>
2013-02-06 05:10:48 -0800

    .

--- config_bash.sh
+++ config_bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i 's/HISTSIZE=1000/HISTSIZE=10000/g' ~/.bashrc
 sed -i 's/HISTFILESIZE=2000/HISTFILESIZE=20000/g' ~/.bashrc
-sed -i 's/HISTCONTROL=ignoreboth/HISTCONTROL=ignoreboth:erasedups\/nHISTTIMEFORMAT="%h %d %H:%M:%S> "/g' ~/.bashrc
+sed -i 's/HISTCONTROL=ignoreboth/HISTCONTROL=ignoreboth:erasedups/nHISTTIMEFORMAT="%h %d %H:%M:%S> "/g' ~/.bashrc
